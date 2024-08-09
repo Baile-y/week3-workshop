55ce0bf703ce55b5aa7d82429c60a33956aca94a
Baile-y <baileybingwilson@gmail.com>
2024-08-09 10:00:39 +1000

    modified:   task3/routes/loginroute.js 	modified:   task4/public/styles.css 	modified:   task4/views/login.html

--- task3/routes/loginroute.js
+++ task3/routes/loginroute.js
@@ -1,20 +1,19 @@
 module.exports = {
-    route: (app) => {
-      const users = [
-        { email: 'email1@com.au', password: '123' },
-        { email: 'email2@com.au', password: '456' },
-        { email: 'email3@com.au', password: '789' }
-      ];
-  
-      app.post('/login', (req, res) => {
-        const { email, password } = req.body;
-        const user = users.find(u => u.email === email && u.password === password);
-        if (user) {
-          res.json({ valid: true });
-        } else {
-          res.json({ valid: false });
-        }
-      });
-    }
-  };
-  
+  route: (app) => {
+    const users = [
+      { email: 'email1@com.au', password: '123' },
+      { email: 'email2@com.au', password: '456' },
+      { email: 'email3@com.au', password: '789' }
+    ];
+
+    app.post('/login', (req, res) => {
+      const { email, password } = req.body;
+      const user = users.find(u => u.email === email && u.password === password);
+      if (user) {
+        res.json({ valid: true });
+      } else {
+        res.json({ valid: false });
+      }
+    });
+  }
+};
